4bbddde837fc6a2c07bb73ff44084f43b3a04096
Sourav Gope <souravgope03@gmail.com>
2026-06-14 13:13:21 +0530

    fix small smtp 4

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -212,13 +212,18 @@ async function sendPasswordResetEmail(userEmail, resetUrl) {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+      // host: "smtp.gmail.com",
+      // port: 587,
+      // secure: false,
+      // auth: {
+      //   user: process.env.EMAIL_USER,
+      //   pass: process.env.EMAIL_PASS
+      // }
+       service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
     });
 
     const mailOptions = {
